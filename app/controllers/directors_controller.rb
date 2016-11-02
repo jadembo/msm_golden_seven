1facfab57b970d3253e92801c90fc1ef8a40673b
Justin A. Dembo <jdembo@chicagobooth.edu>
2016-11-01 20:40:15 -0500

    Enable Directors CRUD

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,52 @@
+class DirectorsController < ApplicationController
+  def index
+    @directors = Director.all
+  end
+
+  def show
+    @director = Director.find(params[:id])
+  end
+
+  def delete
+    @director= Director.find(params[:id])
+    @director.destroy
+
+    redirect_to "/directors"
+  end
+
+  def edit
+    @director = Director.find(params[:id])
+  end
+
+  def update
+    @director = Director.find(params[:id])
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+    @director.save
+
+    redirect_to "/directors/" + params[:id].to_s
+  end
+
+  def new_form
+  end
+
+  def create
+    @director = Director.new
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+    @director.save
+
+    redirect_to "/directors/" + @director.id.to_s
+  end
+
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  
+  #Directors
+  get('/directors', { :controller => 'directors', :action => 'index' })
+  get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show' })
+  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit' })
+  get('/delete_director/:id', { :controller => 'directors', :action => 'delete' })
+  get('/update_director/:id', { :controller => 'directors', :action => 'update' })
+  get('/create_director/', { :controller => 'directors', :action => 'create' })
+
+
 end
